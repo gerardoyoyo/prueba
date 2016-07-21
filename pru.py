24045bd567b856ff7aa64d8332741f97bba1299b
gerardoyoyo <gerardoyo1234321@gmail.com>
2016-07-21 18:08:56 -0400

    aprendiendo python

--- pru.py
+++ pru.py
@@ -1,2 +1,17 @@
-print "hola mundo"
-print "en el master"
+print "inicio en Python"
+i=4
+if 5<i<56:
+	print 'seeee'
+elif i>3:
+	print "menor a tres"
+else:
+	print 'jojojo'
+def suma(a,b):
+	return a+b
+
+print suma(4,5)
+
+print [4,'hola',True]
+
+obj={'a':123,'b':'jajaja'}
+print obj['a']
